c65fd33d16c1fdf09a9b871b429328eedd355df3
dodosh8 <domagoj.nist@gmail.com>
2023-02-22 20:01:06 +0100

    .

--- 05 - React/DEMO/src/Components/ClassComponent.jsx
+++ 05 - React/DEMO/src/Components/ClassComponent.jsx
@@ -1,9 +1,8 @@
-import React from "react";
+// Moramo importati React kada radimo Class komponente, inaće React.Component neće raditi (react nije definiran)
+import React from 'react';
 
-class ClassComponent extends React.Component {
-  render() {
-    return <p>Class component</p>;
-  }
+export default class ClassComponent extends React.Component {
+    render() {
+        return <h2>Hello, friend!</h2>
+    }
 }
-
-export default ClassComponent;
--- 05 - React/DEMO/src/Components/index.js
+++ 05 - React/DEMO/src/Components/index.js
@@ -3,5 +3,3 @@ export { default as Program } from './Program';
 export { default as Student } from './Student';
 export { default as Person } from './Student';
 export { default as Conditional } from './Conditional';
-export { default as ClassComponent } from './ClassComponent';
-export { default as FunctionComponent } from './FunctionComponent';
